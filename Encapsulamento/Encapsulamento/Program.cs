b51ca07e9cf1f2db9f69379c3e0f8a5c13acd3d5
Gustavo Gomes <gustavogomesfx@gmail.com>
2019-06-25 16:59:45 -0300

    Encapsulamento

---
+++ Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulamento {
+    class Program {
+        static void Main(string[] args) {
+
+            Produto produto = new Produto("TV", 500.00, 10);
+
+            produto.SetNome("TV 4K");
+
+            Console.WriteLine(produto.GetNome());
+            Console.WriteLine(produto.GetPreco());
+        }
+    }
+}
